693fe2be1f2709e995cf91b162639419b7549f77
Johannes Spielmann <js@shezi.de>
2013-01-17 14:06:32 +0100

    add automatic logging to log_event

--- eventlog/models.py
+++ eventlog/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 
 import jsonfield
 
+import logging
+logger = logging.getLogger('eventlog')
 
 
 class Log(models.Model):
@@ -20,11 +22,21 @@ class Log(models.Model):
         ordering = ["-timestamp"]
 
 
-def log_event(action, user=None, extra=None):
-    """Log an event that has happened and attach extra information to it."""
+def log_event(action, user=None, extra=None, loglevel=None):
+    """Log an event that has happened and attach extra information to it.
+    
+    If a loglevel is giving, the event will additionally be logged through the standard
+    Django logging system at that level.
+    """
     if user is not None and not user.is_authenticated():
         user = None
     if extra is None:
         extra = {}
+    
+    if loglevel is not None:
+        if user:
+            logger.log(loglevel, "%s (user %s) - %s" % (action, user.username, str(extra)))
+        else:
+            logger.log(loglevel, "%s - %s" % (action, str(extra)))
     
     return Log.objects.create(user=user, action=action, extra=extra)
